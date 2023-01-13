9a590801cf38e28e2e2cbfbb8b567de8901478f7
a-shashkin <caezar1993@gmail.com>
2023-01-13 11:26:23 +0400

    Implemented tests regarding calculations.

--- src/test/java/tests/CalculationOperationsTests.java
+++ src/test/java/tests/CalculationOperationsTests.java
@@ -2,7 +2,6 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,9 +13,9 @@ public class CalculationOperationsTests extends TestBase {
             "1, 1"
     })
     @ParameterizedTest
-    void additionOfTwoOperationTest(Integer number1, Integer number2) {
+    void additionOfTwoNumbersTest(Integer number1, Integer number2) {
 
-        Integer sum = number1 + number2;
+        Integer result = number1 + number2;
 
         $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
                 .$(MobileBy.className("android.widget.EditText"))
@@ -29,6 +28,72 @@ public class CalculationOperationsTests extends TestBase {
 
         $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
                 .$(MobileBy.className("android.widget.EditText"))
-                .shouldHave(Condition.text(sum.toString()));
+                .shouldHave(Condition.text(result.toString()));
+    }
+
+    @CsvSource(value = {
+            "2, 1"
+    })
+    @ParameterizedTest
+    void subtractionOfTwoNumbersTest(Integer number1, Integer number2) {
+
+        Integer result = number1 - number2;
+
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .setValue(number1.toString());
+        $(MobileBy.id("com.candl.athena:id/minus")).click();
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .setValue(number2.toString());
+        $(MobileBy.id("com.candl.athena:id/equal")).click();
+
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .shouldHave(Condition.text(result.toString()));
+    }
+
+    @CsvSource(value = {
+            "2, 2"
+    })
+    @ParameterizedTest
+    void multiplicationOfTwoNumbersTest(Integer number1, Integer number2) {
+
+        Integer result = number1 * number2;
+
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .setValue(number1.toString());
+        $(MobileBy.id("com.candl.athena:id/mul")).click();
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .setValue(number2.toString());
+        $(MobileBy.id("com.candl.athena:id/equal")).click();
+
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .shouldHave(Condition.text(result.toString()));
+    }
+
+    @CsvSource(value = {
+            "4, 2"
+    })
+    @ParameterizedTest
+    void divisionOfTwoNumbersTest(Integer number1, Integer number2) {
+
+        Integer result = number1 / number2;
+
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .setValue(number1.toString());
+        $(MobileBy.id("com.candl.athena:id/div")).click();
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .setValue(number2.toString());
+        $(MobileBy.id("com.candl.athena:id/equal")).click();
+
+        $(MobileBy.id("com.candl.athena:id/layout_input_holder"))
+                .$(MobileBy.className("android.widget.EditText"))
+                .shouldHave(Condition.text(result.toString()));
     }
 }
